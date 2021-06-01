136222c41223cffb77f297abe72fbcb9d4027e7c
eswaribala <parameswaribala@gmail.com>
2021-06-01 16:34:46 +0530

    updated

--- customerapicloud/src/main/java/com/boa/customerapi/controllers/CustomerController.java
+++ customerapicloud/src/main/java/com/boa/customerapi/controllers/CustomerController.java
@@ -3,6 +3,8 @@ package com.boa.customerapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,13 +23,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.bohnman.squiggly.Squiggly;
 import com.github.bohnman.squiggly.util.SquigglyUtils;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/customers")
+@RefreshScope
+@Slf4j
 public class CustomerController {
 
 	@Autowired
 	private CustomerService customerService;
-	
+	@Value("${message}")
+	private String message;
 	@PostMapping({"/v1.0", "/v1.1"})
 	public ResponseEntity<?> addCustomer(@RequestBody Customer customer){
 		
@@ -41,6 +48,7 @@ public class CustomerController {
 	
 	@GetMapping({"/v1.0", "/v1.1"})
 	public List<Customer> getAllCustomers(){
+		log.info(message);
 		return this.customerService.getAllCustomers();
 	}
 	
