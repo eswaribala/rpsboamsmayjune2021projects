764f9881138f07c62ed0acae4902d6a27e1707df
eswaribala <parameswaribala@gmail.com>
2021-06-04 13:21:54 +0530

    RM API

---
+++ rmapiconsumer/src/main/java/com/boa/rmapiconsumer/RmapiconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.boa.rmapiconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmapiconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmapiconsumerApplication.class, args);
+	}
+
+}
